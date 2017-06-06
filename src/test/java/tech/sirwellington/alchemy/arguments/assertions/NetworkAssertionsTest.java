f7205b02ffb917b87b84086eae5f1b58c0a37a17
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-05 17:00:44 -0700

    Updates NetworkAssertionsTest

--- src/test/java/tech/sirwellington/alchemy/arguments/assertions/NetworkAssertionsTest.java
+++ src/test/java/tech/sirwellington/alchemy/arguments/assertions/NetworkAssertionsTest.java
@@ -17,42 +17,37 @@
 package tech.sirwellington.alchemy.arguments.assertions;
 
 import java.net.URL;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
-import tech.sirwellington.alchemy.arguments.FailedAssertionException;
-import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
-import tech.sirwellington.alchemy.test.junit.runners.GenerateInteger;
-import tech.sirwellington.alchemy.test.junit.runners.GenerateURL;
-import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+import tech.sirwellington.alchemy.test.junit.runners.*;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.Get.one;
 import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
 import static tech.sirwellington.alchemy.generator.NumberGenerators.negativeIntegers;
 import static tech.sirwellington.alchemy.generator.StringGenerators.alphanumericString;
-import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
 import static tech.sirwellington.alchemy.test.junit.runners.GenerateInteger.Type.RANGE;
 
 
 /**
- *
  * @author SirWellington
  */
 @Repeat(1000)
 @RunWith(AlchemyTestRunner.class)
-public class NetworkAssertionsTest 
+public class NetworkAssertionsTest
 {
     @GenerateURL
     private URL url;
-    
+
     private static final int MAX_PORT = 65535;
-    
+
     @GenerateInteger(value = RANGE, min = 1, max = MAX_PORT)
     private int port;
-    
+
     @Before
     public void setUp()
     {
@@ -63,13 +58,19 @@ public class NetworkAssertionsTest
     {
         AlchemyAssertion<String> assertion = NetworkAssertions.validURL();
         assertThat(assertion, notNullValue());
-        
+
         assertion.check(url.toString());
-        
+
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidURL() throws Exception
+    {
+        AlchemyAssertion<String> assertion = NetworkAssertions.validURL();
         String badUrl = one(alphanumericString(100));
-        
-        assertThrows(() -> assertion.check(badUrl))
-            .isInstanceOf(FailedAssertionException.class);
+
+        assertion.check(badUrl);
     }
 
     @Test
@@ -77,16 +78,24 @@ public class NetworkAssertionsTest
     {
         AlchemyAssertion<Integer> assertion = NetworkAssertions.validPort();
         assertThat(assertion, notNullValue());
-        
+
         assertion.check(port);
-        
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidPort1() throws Exception
+    {
+        AlchemyAssertion<Integer> assertion = NetworkAssertions.validPort();
         int negative = one(negativeIntegers());
-        assertThrows(() -> assertion.check(negative))
-            .isInstanceOf(FailedAssertionException.class);
-        
+        assertion.check(negative);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidPort2() throws Exception
+    {
+        AlchemyAssertion<Integer> assertion = NetworkAssertions.validPort();
         int tooHigh = one(integers(MAX_PORT + 1, Integer.MAX_VALUE));
-        assertThrows(() -> assertion.check(tooHigh))
-            .isInstanceOf(FailedAssertionException.class);
+        assertion.check(tooHigh);
     }
 
 }
