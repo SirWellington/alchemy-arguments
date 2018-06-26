0d6e9b682f7bb0abae50e241d9a482f089958c77
Wellington Moreno <wellington@censio.love>
2018-06-26 05:37:53 -0700

    Updates copyright headers

--- src/test/java/tech/sirwellington/alchemy/arguments/AlchemyAssertionTest.kt
+++ src/test/java/tech/sirwellington/alchemy/arguments/AlchemyAssertionTest.kt
@@ -1,11 +1,10 @@
 /*
  * Copyright © 2018. Sir Wellington.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  *
  * You may obtain a copy of the License at
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,7 +21,8 @@ import org.junit.runner.RunWith
 import org.mockito.Mockito.never
 import org.mockito.Mockito.verify
 import org.mockito.Spy
-import tech.sirwellington.alchemy.arguments.assertions.*
+import tech.sirwellington.alchemy.arguments.assertions.and
+import tech.sirwellington.alchemy.arguments.assertions.combine
 import tech.sirwellington.alchemy.generator.AlchemyGenerator
 import tech.sirwellington.alchemy.generator.CollectionGenerators
 import tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows
--- src/test/java/tech/sirwellington/alchemy/arguments/ArgumentsTest.kt
+++ src/test/java/tech/sirwellington/alchemy/arguments/ArgumentsTest.kt
@@ -1,11 +1,10 @@
 /*
  * Copyright © 2018. Sir Wellington.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  *
  * You may obtain a copy of the License at
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,7 +19,7 @@ import org.junit.Assert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import tech.sirwellington.alchemy.arguments.Arguments.checkThat
-import tech.sirwellington.alchemy.arguments.assertions.*
+import tech.sirwellington.alchemy.arguments.assertions.nonEmptyString
 import tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner
 import tech.sirwellington.alchemy.test.junit.runners.DontRepeat
