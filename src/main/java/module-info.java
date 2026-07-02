b70dffaf1efaf4188525ab2c76649d33f8b90eb4
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-07-01 17:45:41 -0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,7 @@ module tech.sirwellington.alchemy.arguments {
     requires java.base;
     requires org.slf4j;
     requires tech.sirwellington.alchemy.annotations;
+
+    exports tech.sirwellington.alchemy.arguments;
+    exports tech.sirwellington.alchemy.arguments.assertions;
 }
