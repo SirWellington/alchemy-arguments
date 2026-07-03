64c733a4b45eec80c0d5f8d8860ebb12a1ec5550
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-07-03 16:51:24 -0700

    Refines
    Signed-off-by: Wellington Moreno <JWellington.Moreno@Gmail.com>

--- src/test/java/tech/sirwellington/alchemy/arguments/TestHelpers.java
+++ src/test/java/tech/sirwellington/alchemy/arguments/TestHelpers.java
@@ -1,10 +1,24 @@
 package tech.sirwellington.alchemy.arguments;
 
+import tech.sirwellington.alchemy.annotations.arguments.Required;
+import tech.sirwellington.alchemy.arguments.internal.Checks;
 import tech.sirwellington.alchemy.test.ExceptionOperation;
 import tech.sirwellington.alchemy.test.ThrowableAssertion;
+
+import java.util.List;
+
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.Get.one;
+import static tech.sirwellington.alchemy.generator.NumberGenerators.integers;
 
 public class TestHelpers {
     public static ThrowableAssertion assertThrowsFailedAssertion(ExceptionOperation op) {
         return ThrowableAssertion.assertThrows(op).isInstanceOf(FailedAssertionException.class);
+    }
+
+    @Required
+    public static <E> E randomElementFrom(@Required List<E> list) {
+        Checks.checkNotNullOrEmpty(list);
+        var index = one(integers(0, list.size()));
+        return list.get(index);
     }
 }
